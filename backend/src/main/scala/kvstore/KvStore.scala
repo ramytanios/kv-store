dd40fa53724539acd3ee84dee172c7a57c8d0678
Ramy Tanios <ramy.tanios@gmail.com>
2023-06-03 23:27:23 +0200

    basic kv store modelling

--- backend/src/main/scala/kvstore/KvStore.scala
+++ backend/src/main/scala/kvstore/KvStore.scala
@@ -1,1 +1,54 @@
 package kvstore
+
+import cats.effect._
+import cats.effect.implicits._
+import cats.implicits._
+
+trait KvStore[F[_], K, V] {
+
+  /** update the store with the given key value pair */
+  def put(key: K, value: V): F[Unit]
+
+  /** get a value based on a key from store */
+  def get(key: K): F[V]
+
+  /** list of all key-value pairs */
+  def kvs: F[List[(K, V)]]
+
+  /** number of keys */
+  def nKeys: F[Int]
+}
+
+object KvStore {
+
+  class KvStoreMissingKeyException(msg: String) extends RuntimeException(msg)
+
+  def make[F[_], K, V](implicit
+      F: Concurrent[F]
+  ): Resource[F, KvStore[F, K, V]] =
+    for {
+      store <- F.ref(Map.empty[K, V]).toResource
+    } yield new KvStore[F, K, V] {
+
+      override def put(key: K, value: V): F[Unit] =
+        store.update(_.updated(key, value))
+
+      override def get(key: K): F[V] =
+        store.get
+          .map(_.get(key))
+          .flatMap { valueOpt =>
+            F.fromOption(
+              valueOpt,
+              new KvStoreMissingKeyException(
+                s"Key $key does not exist in the store."
+              )
+            )
+          }
+
+      override def kvs: F[List[(K, V)]] = store.get.map(_.toList)
+
+      override def nKeys: F[Int] = store.get.map(_.size)
+
+    }
+
+}
