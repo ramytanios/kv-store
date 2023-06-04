96be7937d8a6cc1dc9a80d43b9c36ae03411c5c6
Ramy Tanios <ramy.tanios@gmail.com>
2023-06-04 10:12:08 +0200

    sketch of routes

---
+++ backend/src/main/scala/kvstore/Routes.scala
@@ -0,0 +1,63 @@
+package kvstore
+
+import org.http4s.dsl.Http4sDsl
+import org.http4s.HttpRoutes
+import org.http4s.server.Router
+import cats._
+import org.http4s.EntityEncoder
+import org.http4s.circe._
+import org.http4s.EntityDecoder
+import io.circe._
+import io.circe.generic.semiauto._
+import cats.effect._
+import cats.effect.implicits._
+import cats.implicits._
+
+object Routes {
+
+  /** Backend alive http endpoints */
+  final class AliveRoutes[F[_]: Monad]() extends Http4sDsl[F] {
+    private val prefixPath = "/alive"
+
+    private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
+      case GET -> Root =>
+        Ok("I am alive")
+    }
+
+    val routes: HttpRoutes[F] = Router(prefixPath -> httpRoutes)
+  }
+
+  /** Key value store http endpoints */
+  final class KvStoreRoutes[F[_]: Concurrent](store: KvStore[F, String, String])
+      extends Http4sDsl[F] {
+    private val prefixPath = "/store"
+
+    // TODO: Move to dtos
+    case class KeyValue(key: String, value: String)
+    object KeyValue {
+      implicit val codec: Codec[KeyValue] = deriveCodec
+    }
+
+    implicit val keyValueEntityEncoder: EntityEncoder[F, List[KeyValue]] =
+      jsonEncoderOf[F, List[KeyValue]]
+
+    implicit val keyValueEntityDecoder: EntityDecoder[F, KeyValue] =
+      jsonOf[F, KeyValue]
+
+    private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
+      case GET -> Root =>
+        Ok(store.entries.map(_.map { case (key, value) =>
+          KeyValue(key, value)
+        }))
+      case req @ POST -> Root =>
+        for {
+          keyValue <- req.as[KeyValue]
+          _ <- store.put(keyValue.key, keyValue.value)
+          response <- Ok()
+        } yield response
+    }
+
+    val routes: HttpRoutes[F] = Router(prefixPath -> httpRoutes)
+  }
+
+}
--- backend/src/main/scala/kvstore/Service.scala
+++ backend/src/main/scala/kvstore/Service.scala
@@ -1,1 +1,28 @@
 package kvstore
+
+import cats.effect.IOApp
+import cats.effect.{ExitCode, IO}
+import org.http4s.server.Router
+import cats.syntax.semigroupk._
+import org.http4s.ember.server._
+import com.comcast.ip4s._
+import org.http4s.HttpRoutes
+import cats.effect.Async
+import cats.data.Kleisli
+import org.http4s.{Request, Response}
+
+object Service extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"8090")
+      .build
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
+
+  }
+
+}
